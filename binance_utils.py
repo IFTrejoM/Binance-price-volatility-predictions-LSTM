c44522872419d6f5f9decb3c9dcef3e74303e27a
IFTrejoM <71332826+IFTrejoM@users.noreply.github.com>
2023-10-09 13:24:22 -0500

    No improvements with the new target_var

--- binance_utils.py
+++ binance_utils.py
@@ -489,3 +489,45 @@ def cross_validated_lle(data, n=10000, cv=5, emb_dim=10, min_tsep=250, lag=1052,
         # print(f"LLE for segment starting at index {start_idx}: {lle}")
     
     return lles
+
+#####
+
+import nolds
+import numpy as np
+
+def cross_validated_dfa(data, n=10000, cv=5, order=1, seed=None):
+    """
+    Realiza una validación cruzada del Detrended Fluctuation Analysis (DFA) en una serie temporal.
+    
+    Parámetros:
+    - data: Serie temporal para analizar.
+    - n: Número de observaciones a seleccionar en cada iteración de validación cruzada.
+    - cv: Número de segmentos en los que dividir los datos.
+    - order: Orden del polinomio utilizado para detrendar los datos en el DFA.
+    - seed: Semilla para la generación de números aleatorios.
+    
+    Retorna:
+    - alphas: Lista de exponentes de escala (alfa) calculados en cada iteración de validación cruzada.
+    """
+    alphas = []
+    
+    # Asegurarse de que los datos tienen al menos n puntos
+    assert len(data) >= n, "Not enough data points for the specified n"
+    
+    # Establecer la semilla para la generación de números aleatorios
+    np.random.seed(seed)
+    
+    for _ in range(cv):
+        # Selecciona un índice de inicio aleatorio para el segmento de datos
+        start_idx = np.random.randint(0, len(data) - n + 1)
+        
+        # Selecciona un segmento de datos de tamaño n
+        segment = data[start_idx:start_idx+n]
+        
+        # Calcula el alfa (exponente de escala) para el segmento y lo añade a la lista de alfas
+        alfa = nolds.dfa(segment, order=order)
+        alphas.append(alfa)
+        
+        # print(f"Alfa for segment starting at index {start_idx}: {alfa}")
+    
+    return alphas
